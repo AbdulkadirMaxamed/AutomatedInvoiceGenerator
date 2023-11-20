206726cc69f41d27e2693f181979bb8d59f5a1ab
hackerabdul <abdulkadir.q12@gmail.com>
2023-11-20 17:28:18 +0000

    verifiying server runs in

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require('mongoose');
 
-//setting up user route for admin authentication
+// //setting up user route for admin authentication
 const userRoute = require('./routes/users');
 
 mongoose.connect(`mongodb+srv://abdulkadirq12:InvoiceGenerator@cluster0.kynpuci.mongodb.net/`,{
@@ -12,7 +12,11 @@ mongoose.Promise = global.Promise;
 
 app.use(express.json());
 
-app.use('/users', userRoute);
-
+// app.use('/users', userRoute);
+app.use((req,res,next) =>{
+    res.status(200).json({
+        message: 'server is currently running'
+    })
+})
 
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -2,3 +2,6 @@
 //according to the scope we only require 1 user "admin"
 //a basic connection to authenticate if the admin user is logged in
 //will need to consider validation and password encryption
+
+const express = require('express')
+const router = express.Router
