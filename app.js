4533b2eb86c124bf30c70ebb35ca5962e7a79615
hackerAbdul <abdulkadir.q12@gmail.com>
2023-11-21 21:58:28 +0000

    adding cars route for getting cars info updating app.js to contain this

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 
 // //setting up user route for admin authentication
 const userRoute = require('./routes/users');
+const carsRoute = require('./routes/cars');
 
 mongoose.connect(`mongodb+srv://abdulkadirq12:InvoiceGenerator@cluster0.kynpuci.mongodb.net/`,{
     useNewUrlParser: true
@@ -12,7 +13,7 @@ mongoose.Promise = global.Promise;
 
 app.use(express.json());
 
-// app.use('/users', userRoute);
+app.use('/cars', carsRoute);
 app.use((req,res,next) =>{
     res.status(200).json({
         message: 'server is currently running'
