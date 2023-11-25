d255d66d7424d57ec8a0fd1cd93bfd23cae7bce4
hackerAbdul <abdulkadir.q12@gmail.com>
2023-11-25 21:42:56 +0000

    Handling CORs error and adding the add new car feature

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const mongoose = require('mongoose');
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
 
 // //setting up user route for admin authentication
 const userRoute = require('./routes/users');
@@ -11,6 +13,25 @@ mongoose.connect(`mongodb+srv://abdulkadirq12:InvoiceGenerator@cluster0.kynpuci.
     useNewUrlParser: true
 });
 mongoose.Promise = global.Promise;
+
+app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({
+    extended: false,
+}))
+app.use(bodyParser.json())
+
+//setting ACAO
+app.use((req,res,next) =>{
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Header",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+
+    if(req.method === "OPTIONS"){
+        res.header("Access-Control-Allow-Methods", "PUT, POST, GET, PATCH, DELETE")
+        return res.status(200).json({})
+    }
+    next()
+})
 
 app.use(express.json());
 
@@ -30,17 +51,24 @@ app.use('/orders', orderRoute);
 app.use('/users', userRoute);
 
 app.use('/cars', carsRoute);
+
 app.use((req,res,next) =>{
     res.status(200).json({
         message: 'server is currently running'
     })
 })
 
-// takes error variable from above and outputs error message as JSON
-app.use((error, req, res, next) => {
-    res.status(error.status || 500);
+//throws an error if database connection fails
+app.use((req,res,next) =>{
+    const error = new Error('Not Found')
+    error.status = 404
+    next(error)
+})
+
+app.use((error, req,res,next) =>{
+    res.status(err.status || 500)
     res.json({
-        error: {
+        error:{
             message: error.message
         }
     })
