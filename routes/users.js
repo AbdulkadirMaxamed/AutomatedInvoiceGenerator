5d104714f900c6b52aedadc55e8fec7e85b85dae
hackerabdul <abdulkadir.q12@gmail.com>
2023-12-08 00:23:06 +0000

    solving merge conflict

--- routes/users.js
+++
@@ -1,7 +0,0 @@
-//Abeer this is where your user route connection should.
-//according to the scope we only require 1 user "admin"
-//a basic connection to authenticate if the admin user is logged in
-//will need to consider validation and password encryption
-
-const express = require('express')
-const router = express.Router()
